4dc3f33a21f0c6980a3fefd36816f7093018b128
Stephan Hochhaus <stephan@yauh.de>
2015-04-25 18:59:29 +0200

    remove showHouse helpers to conclude refactoring

--- houseSitter2/client/client.js
+++ houseSitter2/client/client.js
@@ -42,13 +42,6 @@ Template.selectHouse.events({
   }
 });
 
-Template.showHouse.helpers({
-  house: function () {
-    return HousesCollection.findOne({
-      _id: Session.get('selectedHouseId')
-    });
-  }
-});
 Template.showHouse.events({
   'click button#delete': function (evt) {
     var id = this._id;
